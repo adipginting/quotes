63f8bd69d47547be2f55a86c6136d2ac51def8e7
adipginting <apg.foss@gmail.com>
2023-07-21 00:30:16 +0700

    fix: simplify file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuoteModule } from './quote/quote.module';
 
 @Module({
-  imports: [QuoteModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
