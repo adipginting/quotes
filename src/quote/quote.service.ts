0eced07f7053d5e1fa76307f1253dd48789bf739
adipginting <apg.foss@gmail.com>
2023-07-20 18:50:01 +0700

    fix: amended empty quote with recursive function

--- src/quote/quote.service.ts
+++ src/quote/quote.service.ts
@@ -5,17 +5,25 @@ import * as fs from 'fs';
 @Injectable()
 export class QuoteService {
   getQuote(): string {
-    let randomBookNumber: number = Math.floor(Math.random() * 12);
-    if (randomBookNumber === 0) {
-      randomBookNumber = 1;
+    function processQuote(): string {
+      let randomBookNumber: number = Math.floor(Math.random() * 12);
+      if (randomBookNumber === 0) {
+        randomBookNumber = 1;
+      }
+      const html: string = fs.readFileSync(`marcus-aurelius_meditations_george-long/src/epub/text/book-${randomBookNumber}.xhtml`, 'utf8');
+      let text: string = convert(html, { wordwrap: false });
+      text = text.replace(/\[.*?\]/g, "");
+      text = text.replace(/[0-9]/g, "");
+      let textArray: Array<string> = text.split('.');
+      const randomQuoteNumber: number = Math.floor(Math.random() * textArray.length);
+      let quote: string = textArray[randomQuoteNumber];
+      if (quote !== "" || quote !== undefined) {
+        return quote + ".";
+      } else {
+        return processQuote();
+      }
     }
-    const html: string = fs.readFileSync(`marcus-aurelius_meditations_george-long/src/epub/text/book-${randomBookNumber}.xhtml`, 'utf8');
-    let text: string = convert(html, { wordwrap: false });
-    text = text.replace(/\[.*?\]/g, "");
-    text = text.replace(/[0-9]/g, "");
-    let textArray: Array<string> = text.split('.');
-    const randomQuoteNumber: number = Math.floor(Math.random() * textArray.length);
-    const quote: string = textArray[randomQuoteNumber] + ".";
-    return quote;
+
+    return processQuote();
   }
 }
